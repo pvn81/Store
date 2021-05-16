e6e00c03b308837a31861c2cc8da46ea5014bd6d
pvn1981 <v_pechikin1981@mail.ru>
2021-05-16 16:48:10 +0200

    Описать сущьность Detail

---
+++ domain/Store/Detail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Store
+{
+    public class Detail
+    {
+        public int Id { get; set; }
+        public string DetName { get; }
+
+        public Detail( int id, string detName)
+        {
+            this.Id = id;
+            this.DetName = detName;
+        }
+    }
+}
